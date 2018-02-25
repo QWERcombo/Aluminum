2f48e5e64ab9c846a969d438ccb3b8207e25819f
QWERcombo <850145740@qq.com>
2018-02-25 15:37:01 +0800

    Initial Commit

---
+++ IONMarketTest/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  IONMarketTest
+//
+//  Created by 赵越 on 2018/2/25.
+//  Copyright © 2018年 赵越. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ IONMarketTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  IONMarketTest
+//
+//  Created by 赵越 on 2018/2/25.
+//  Copyright © 2018年 赵越. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
