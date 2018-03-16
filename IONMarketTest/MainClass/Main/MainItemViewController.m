2501543586af48e7a6eb071a7e12717b77852573
Unknown <850145740@qq.com>
2018-03-16 23:08:32 +0800

    20180316

--- IONMarketTest/MainClass/Main/MainItemViewController.m
+++ IONMarketTest/MainClass/Main/MainItemViewController.m
@@ -12,18 +12,93 @@
 
 @end
 
+#define ITEM_WIDTH  60
+#define ITEM_HEIGHT  30
+
 @implementation MainItemViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.title = self.titleStr;
+    self.title = @"下单";
+    
+    UIButton *rightButton = [UIButton buttonWithTitle:@"特殊定制" andFont:FONT_ArialMT(15) andtitleNormaColor:[UIColor whiteColor] andHighlightedTitle:[UIColor whiteColor] andNormaImage:nil andHighlightedImage:nil];
+    rightButton.tag = 1010;
+    [rightButton addTarget:self action:@selector(buttonCliker:) forControlEvents:UIControlEventTouchUpInside];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:rightButton];
+    
+    self.view.backgroundColor = [UIColor mianColor:1];
+    
+    [self setSubviews];
+}
+
+#pragma mark --- SubViews
+
+- (void)setSubviews {
+    
+    [self createTopActionView];
+    
+    [self createScrollLayoutView];
+}
+
+- (void)createTopActionView {
+    UIView *topView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIGHT, 100)];
+    topView.backgroundColor = [UIColor whiteColor];
+    UIView *line = [[UIView alloc] initWithFrame:CGRectMake(0, 49.5, SCREEN_WIGHT, 1)];
+    line.backgroundColor = [UIColor lightTextColor];
+    [topView addSubview:line];
+    
+    NSArray *array = @[@"整板",@"零切",@"圆棒",@"型材",@"管材"];
+    UIButton *lastBtn = nil;
+    for (NSInteger i=0; i<5; i++) {
+        
+        UIButton *button = [UIButton buttonWithTitle:[array objectAtIndex:i] andFont:FONT_ArialMT(15) andtitleNormaColor:[UIColor mianColor:2] andHighlightedTitle:[UIColor whiteColor] andNormaImage:nil andHighlightedImage:nil];
+        
+        if (i==0) {
+            [button setHighlighted:YES];
+        }
+        
+        [button setBackgroundImage:[UIImage imageWithColor:[UIColor mianColor:2]] forState:UIControlStateHighlighted];
+//        button.backgroundColor = [UIColor mianColor:2];
+        [button addTarget:self action:@selector(buttonCliker:) forControlEvents:UIControlEventTouchUpInside];
+        
+        button.tag = 100+i;
+        
+        [topView addSubview:button];
+        
+        [button mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.width.equalTo(@(ITEM_WIDTH));
+            make.height.equalTo(@(ITEM_HEIGHT));
+            make.top.equalTo(topView.mas_top).offset(10);
+            if (lastBtn) {
+                make.left.equalTo(lastBtn.mas_right).offset((SCREEN_WIGHT-ITEM_WIDTH*5)/6);
+            } else {
+                make.left.equalTo(topView.mas_left).offset((SCREEN_WIGHT-ITEM_WIDTH*5)/6);
+            }
+        }];
+        
+        lastBtn = button;
+    }
+    
+    
+    
+    [self.view addSubview:topView];
+}
+
+- (void)createScrollLayoutView {
+    
+    
     
 }
 
+#pragma mark --- Action
 
-
-
+- (void)buttonCliker:(UIButton *)sender {
+    NSLog(@"%@", sender.currentTitle);
+    
+    
+    
+}
 
 
 
--- IONMarketTest/MainClass/Mine/设置/ChangeCodeViewController.m
+++ IONMarketTest/MainClass/Mine/设置/ChangeCodeViewController.m
@@ -8,7 +8,14 @@
 
 #import "ChangeCodeViewController.h"
 
-@interface ChangeCodeViewController ()
+@interface ChangeCodeViewController ()<UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *oldTF;
+@property (weak, nonatomic) IBOutlet UITextField *theNewTF;
+@property (weak, nonatomic) IBOutlet UITextField *confirmTF;
+
+@property (nonatomic, strong) NSMutableString *oldCode;
+@property (nonatomic, strong) NSMutableString *theNewCode;
+@property (nonatomic, strong) NSMutableString *confirmCode;
 
 @end
 
@@ -17,18 +24,56 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-//    self.title = @"";
     self.view.backgroundColor = [UIColor mianColor:1];
+    self.oldCode = [NSMutableString string];
+    self.confirmCode = [NSMutableString string];
+    self.theNewCode = [NSMutableString string];
 }
+
+
+
+#pragma mark --- tfdelegate
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    if (textField==self.oldTF) {
+        [self.oldCode replaceCharactersInRange:range withString:string];
+    }
+    
+    if (textField==self.theNewTF) {
+        [self.theNewCode replaceCharactersInRange:range withString:string];
+    }
+    
+    if (textField==self.confirmTF) {
+        [self.confirmCode replaceCharactersInRange:range withString:string];
+    }
+    
+    return YES;
+}
+
+
 
 - (IBAction)changeCode:(id)sender {
     
+    [self checkInputInfomation];
+    
+    __weak typeof(self) weakself = self;
+    
     [[UtilsData sharedInstance] showAlertControllerWithTitle:@"提示" detail:@"是否确定修改密码" doneTitle:@"确定" cancelTitle:@"取消" haveCancel:YES doneAction:^{
+        
+        [[UtilsData sharedInstance] showAlertTitle:@"" detailsText:@"修改成功!" time:2 aboutType:WHShowViewMode_Text state:YES];
+        
+        [weakself.navigationController popViewControllerAnimated:YES];
         
     } controller:self];
     
 }
 
+- (void)checkInputInfomation {
+    NSLog(@"%@", self.oldCode);
+    NSLog(@"%@", self.theNewCode);
+    NSLog(@"%@", self.confirmCode);
+    
+}
 
 
 - (void)didReceiveMemoryWarning {
