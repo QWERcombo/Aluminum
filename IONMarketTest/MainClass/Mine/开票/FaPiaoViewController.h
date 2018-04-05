620be05884a4f5a5d442bbd7bbc5919aa8bcc93f
Unknown <850145740@qq.com>
2018-04-05 17:57:54 +0800

    20180405

---
+++ IONMarketTest/MainClass/Mine/开票/FaPiaoViewController.h
@@ -0,0 +1,13 @@
+//
+//  FaPiaoViewController.h
+//  IONMarketTest
+//
+//  Created by 瓜豆2018 on 2018/4/5.
+//  Copyright © 2018年 赵越. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface FaPiaoViewController : BaseViewController
+
+@end
---
+++ IONMarketTest/Model/Mine/BillTicketModel.h
@@ -0,0 +1,13 @@
+//
+//  BillTicketModel.h
+//  IONMarketTest
+//
+//  Created by 瓜豆2018 on 2018/4/5.
+//  Copyright © 2018年 赵越. All rights reserved.
+//
+
+#import "BaseModel.h"
+
+@interface BillTicketModel : BaseModel
+
+@end
